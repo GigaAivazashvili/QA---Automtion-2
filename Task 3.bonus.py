e8e6684accd041c3d14d39e08928cd86b4e0ef6a
GigaAivazashvili <gaivazashvili@gmail.com>
2023-04-24 02:12:00 +0400

    Home Work

---
+++ Task 3.bonus.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+class some(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get("https://the-internet.herokuapp.com/")
+
+    def test_check_box(self):
+        self.driver.find_element(By.LINK_TEXT, "File Download").click()
+        print(self.driver.get_cookies())
+
+    def tearDown(self) -> None:
+        self.driver.close()
